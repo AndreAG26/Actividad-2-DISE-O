ae541dddbb50fa929763e43bb28d02bf1c83501e
Andrea Alemán <103344222+AndreAG26@users.noreply.github.com>
2024-03-06 21:25:58 -0600

    STAR

---
+++ Actividad 2/MODELO/Maquinaria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2.MODELO
+{
+    internal abstract class Maquinaria
+    {
+        protected double valor;
+        protected int vidaUtil;
+        protected String marca;
+        protected String descricion;
+
+       public Maquinaria(double valor, int vidaUtil, string marca, string descricion)
+        {
+            this.valor = valor;
+            this.vidaUtil = vidaUtil;
+            this.marca = marca;
+            this.descricion = descricion;
+        }
+
+        public abstract String calcularDepreciacion();
+    }
+}
